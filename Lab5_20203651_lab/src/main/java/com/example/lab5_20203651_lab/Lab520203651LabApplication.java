691e2cfe95a0fbbea43ea4abdfd5d1a0f266b200
Pablo <a20203651@pucp.edu.pe>
2023-09-14 20:18:40 -0500

    creando archivo

---
+++ Lab5_20203651_lab/src/main/java/com/example/lab5_20203651_lab/Lab520203651LabApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab5_20203651_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab520203651LabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab520203651LabApplication.class, args);
+	}
+
+}
